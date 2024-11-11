2ca276a7c48d861cdf585bfae9dc20067e7695b0
Kavun <kavun4eg@gmail.com>
2024-11-11 19:47:57 +0200

    add __setattr__

--- Homework15.py
+++ Homework15.py
@@ -1,23 +1,34 @@
 class Romb:
     def __init__(self, сторона_а, кут_а):
+        self.сторона_а = сторона_а
+        self.кут_а = кут_а
+        self.кут_б = 180 - кут_а
 
-        if сторона_а <= 0:
-            raise ValueError("Сторона повинна бути більшою за 0")
+    def __setattr__(self, name, value):
+        if name == 'сторона_а':
+            if value <= 0:
+                raise ValueError("Сторона повинна бути більшою за 0")
+            super().__setattr__(name, value)
 
-        if not (0 < кут_а < 180):
-            raise ValueError("Кут повинен бути в межах від 0 до 180 градусів")
+        elif name == 'кут_а':
+            if not (0 < value < 180):
+                raise ValueError("Кут повинен бути в межах від 0 до 180 градусів")
+            super().__setattr__(name, value)
 
-        setattr(self, 'сторона_а', сторона_а)
-        setattr(self, 'кут_а', кут_а)
+            self.кут_б = 180 - value
 
-        кут_б = 180 - кут_а
-        setattr(self, 'кут_б', кут_б)
+        elif name == 'кут_б':
+            if not (0 < value < 180):
+                raise ValueError("Кут повинен бути в межах від 0 до 180 градусів")
+            if 180 - value != self.кут_а:
+                raise ValueError("Сума кутів повинна бути рівна 180 градусів")
+            super().__setattr__(name, value)
 
     def __str__(self):
-            return (
-                    f"Сторона: {self.сторона_а}\n"
-                    f"Кут 1 (кут_а): {self.кут_а}°\n"
-                    f"Кут 2 (кут_б): {self.кут_б}°")
-
-ромб = Romb(5, 60)
+        return (
+            f"Сторона: {self.сторона_а}\n"
+            f"Кут 1 (кут_а): {self.кут_а}°\n"
+            f"Кут 2 (кут_б): {self.кут_б}°"
+        )
+ромб = Romb(1, 179)
 print(ромб)
